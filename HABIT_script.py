480bb20440fa256eea2ab33601ab0550addecdcb
David Yeung <dyeung156@gmail.com>
2024-11-01 15:16:32 -0400

    Managed to learn how to make slash commands. Tailored for one server tho, but probs not an issue

--- HABIT_script.py
+++ HABIT_script.py
@@ -1,41 +1,58 @@
 import os
 import discord
 from discord.ext import commands
+from discord import app_commands
 from dotenv import load_dotenv
 
 #get the token from env file
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD_ID = os.getenv('DISCORD_GUILD')
 
 #set up intents
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
-client = commands.Bot(command_prefix="#")
+client = commands.Bot(command_prefix="#", intents=intents)
+
+
 
 @client.event
 async def on_ready():
+    try:
+        guild = discord.Object(id = GUILD_ID)
+        synced = await client.tree.sync(guild = guild)
+        print(f"Synced {len(synced)} commands to guild {guild.id}")
+    except Exception as e:
+        print(f"Error syncing commands: {e}")
+        
     print(f'{client.user} has connected to Discord!')
 
-@client.event
-async def on_message(message):
-    username = str(message.author).split("#")[0]
-    channel = str(message.channel.name)
-    user_message = str(message.content)
+# @client.event
+# async def on_message(message):
+#     # print(str(message.author))
+#     username = str(message.author).split("#")[0]
+#     channel = str(message.channel.name)
+#     user_message = str(message.content)
     
-    #check if message was from this bot
-    if message.author == client.user:
-        return
+#     #check if message was from this bot
+#     if message.author == client.user:
+#         return
     
-    print(f"User {username} wrote in {channel}: {user_message}")
+#     print(f"User {username} wrote in {channel}: {user_message}")
     
-    if user_message == "hello":
-        await message.channel.send(f"Hello there {username}")
-        
-@client.command()
-async def ping(ctx):
-    await ctx.send("Pong")
+#     if user_message == "hello":
+#         await message.channel.send(f"Hello there {username}")
 
+GUILD_NUM = discord.Object(id = GUILD_ID)
+@client.tree.command(name = "hello_there", description = "Replys with Hello There", guild = GUILD_NUM)
+async def sayHello(interaction : discord.Integration):
+    await interaction.response.send_message("Hello There")
+
+@client.tree.command(name = "repeat", description = "Replys with Hello There", guild = GUILD_NUM)
+async def sayHello(interaction : discord.Integration, print_out : str):
+    await interaction.response.send_message(print_out)
+    
+    
 #TODO 
-#Set up Git respo for this dumb dumb
+
 client.run(TOKEN)
