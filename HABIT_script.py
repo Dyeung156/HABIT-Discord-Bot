3c477835fa092944ce083c6f26267f1badd4457d
David Yeung <dyeung156@gmail.com>
2024-11-30 16:05:03 -0500

    Updated the text in the select menu to reflect project. COmmiting here for back up

--- HABIT_script.py
+++ HABIT_script.py
@@ -11,6 +11,7 @@ from discord.ext import commands
 
 #import commands from other py files
 import commands_files.menu_commands
+import asyncio
 
 #get the token from env file
 load_dotenv()
@@ -44,11 +45,20 @@ async def save_user_command(user_id, command_name, command_data):
 #returns: the text associated with the command 
 async def get_user_command(user_id, command_name):
     user_data = user_commands_collection.find_one({"user_id": user_id})
+    
     if user_data:
         return user_data.get(command_name)
     else:
         return None
 
+#get all commands from a user's personal collection
+async def get_all_user_commands(user_id):
+    user_data = await user_commands_collection.find_one({"user_id": user_id})  
+    if user_data:
+        # Remove 'user_id' key to return only the commands in a dict format
+        return {key: value for key, value in user_data.items() if key != "user_id" and key != "_id"}
+    else:
+        return None
 
 #set up intents
 intents = discord.Intents.default()
@@ -59,30 +69,44 @@ GUILD_NUM = discord.Object(id = GUILD_ID)
 
 class MyView(discord.ui.View): # Create a class called MyView that subclasses discord.ui.View
     @discord.ui.select( # the decorator that lets you specify the properties of the select menu
-        placeholder = "Choose a Flavor!", # the placeholder text that will be displayed if nothing is selected
+        placeholder = "Choose an action", # the placeholder text that will be displayed if nothing is selected
         min_values = 1, # the minimum number of values that must be selected by the users
         max_values = 1, # the maximum number of values that can be selected by the users
         options = [ # the list of options from which users can choose, a required field
             discord.SelectOption(
-                label="Vanilla",
-                description="Pick this if you like vanilla!"
+                label="Save Command",
+                description="Pick this if you like vanilla!", 
+                value = "save_cmd"
             ),
             discord.SelectOption(
-                label="Chocolate",
-                description="Pick this if you like chocolate!"
+                label="Get Command",
+                description="Pick this if you like chocolate!",
+                value = "get_cmd"
             ),
-            discord.SelectOption(
-                label="Strawberry",
-                description="Pick this if you like strawberry!"
-            )
         ]
     )
-    async def select_callback(self, interaction : discord.Interaction, select : discord.ui.select): # the function called when the user is done selecting options
-        await interaction.response.send_message(f"Awesome! I like {select.values[0]} too!")
+    async def select_callback(self, interaction : discord.Interaction, select : discord.ui.select):
+        action = select.values[0]
+        await interaction.response.send_message(f"Executing {action}", ephemeral = True)
+        
+        def check(message):
+            return message.author == interaction.user and message.channel == interaction.channel
+
+        try:
+            message = await interaction.client.wait_for("message", timeout=30.0, check=check)
+            if action == "save_cmd":
+                # Programmatically call the `command1` slash command
+                await saveCmd.callback(interaction, input_text=message.content)
+            elif action == "get_cmd":
+                # Programmatically call the `command2` slash command
+                await getCmd.callback(interaction, input_text=message.content)
+        except asyncio.TimeoutError:
+            await interaction.followup.send("You didn't provide input in time. Try again!")
+ 
         
     @discord.ui.button(label="", row = 1, style=discord.ButtonStyle.primary, emoji="⬅️") 
     async def button_call(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message("You clicked the button!") # Send a message when the button is clicked
+        await interaction.response.send_message("You clicked the button!") 
         
     @discord.ui.button(label="", row = 1, style=discord.ButtonStyle.primary, emoji="➡️") 
     async def button_callback(self, interaction: discord.Interaction, button: discord.ui.Button):
@@ -103,22 +127,6 @@ async def on_ready():
     
     print(f'{client.user} has connected to Discord!')
 
-# @client.event
-# async def on_message(message):
-#     # print(str(message.author))
-#     username = str(message.author).split("#")[0]
-#     channel = str(message.channel.name)
-#     user_message = str(message.content)
-    
-#     #check if message was from this bot
-#     if message.author == client.user:
-#         return
-    
-#     print(f"User {username} wrote in {channel}: {user_message}")
-    
-#     if user_message == "hello":
-#         await message.channel.send(f"Hello there {username}")
-
 @client.tree.command(name = "hello_there", description = "Replys with Hello There", guild = GUILD_NUM)
 async def sayHello(interaction : discord.Interaction):
     await interaction.response.send_message("Hello There")
@@ -130,7 +138,7 @@ async def repeatPhrase(interaction : discord.Interaction, print_out : str):
 @client.tree.command(name = "save_cmd", description = "Saves a text command for the user", guild = GUILD_NUM)
 async def saveCmd(interaction : discord.Interaction, cmd_name : str, text_output : str):
     await save_user_command(interaction.user.id, cmd_name, text_output)
-    await interaction.response.send_message(f"Command {cmd_name} saved")
+    await interaction.response.send_message(f"Command {cmd_name} saved", ephemeral = True)
 
 @client.tree.command(name = "get_cmd", description = "Retrieves a text command for the user and outputs it", guild = GUILD_NUM)
 async def getCmd(interaction : discord.Interaction, cmd_name : str):
@@ -140,5 +148,12 @@ async def getCmd(interaction : discord.Interaction, cmd_name : str):
         phrase = f"Command {cmd_name} not found"
     await interaction.response.send_message(phrase)
 
+@client.tree.command(name = "get_all_cmd", description = "Retrieves all commands for the user and outputs it", guild = GUILD_NUM)
+async def getCmd(interaction : discord.Interaction):
+    phrase = get_all_user_commands(interaction.user.id)
+    
+    if phrase == None:
+        phrase = f"Commands not found"
+    print(phrase)
 
 client.run(TOKEN)
