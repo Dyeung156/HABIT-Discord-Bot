0974a5505f57de930974f2328a3c9aab1959ca86
David Yeung <dyeung156@gmail.com>
2024-12-04 00:11:12 -0500

    Added a history menu to the main menu that lets the user see their top repeated messages and then select it to save for command

--- HABIT_script.py
+++ HABIT_script.py
@@ -12,7 +12,6 @@ from discord.ext import commands
 
 #for message history 
 from datetime import datetime
-import string
 
 #get the token from env file
 load_dotenv()
@@ -102,6 +101,9 @@ async def get_all_user_commands(user_id):
         return []
 
 #delete a command from a user's persoanl collection
+#parameters: user_id (int) -the user's Discord ID
+#            command_name (str) - name of the command 
+#returns: boolean detailing if anything was deleted
 async def delete_command(user_id, command_name):
     # Define the filter to find the user by user_id
     filter = {"user_id": user_id}
@@ -114,7 +116,36 @@ async def delete_command(user_id, command_name):
     
     # if modified_count > 0, then something was deleted
     return result.modified_count > 0
+
+#finds the top messages that a user has sent in the channel
+#parameters: interaction (discord.interaction) - the discord interaction the user made
+#            user (discord.user) - the discord user this function is looking at 
+#            count (int) - number of messages function should return
+#            date (str)(optional) - date messages should be after 
+#returns: results (list) - sorted list of tuples (message content, number of occurances)
+async def find_top_messages(interaction : discord.Interaction, user : discord.user, count: int, date : str = None):
+# Create the dictionary to hold messages
+    message_dict = dict()
     
+    num_msg = 0
+    # Fetch all messages in the channel after date and organize by first letter
+    async for message in interaction.channel.history(limit=None, after=date):  
+        if message.author == user:
+            
+            if message.content in message_dict:
+                message_dict[message.content] += 1
+            else:
+                message_dict[message.content] = 1
+                
+            num_msg += 1
+            #hard limit to avoid too much sorting
+            if num_msg == 1000: 
+                break
+    
+    # sort the dict and then get the top items with the highest values
+    results = sorted(message_dict.items(), key=lambda item: item[1], reverse=True)[:count]
+    return results
+
 #set up intents
 intents = discord.Intents.default()
 intents.message_content = True
@@ -143,6 +174,11 @@ class MainMenu(discord.ui.View):
                 description="Delete a personal command",
                 value = "delete_cmd"
             ),
+            discord.SelectOption(
+                label="Top Message History Command",
+                description="See most repeated messages. Can save a message as a command by selecting it",
+                value = "history_cmd"
+            ),
         ]
     )
     async def select_callback(self, interaction : discord.Interaction, select : discord.ui.select):
@@ -157,9 +193,12 @@ class MainMenu(discord.ui.View):
         elif action == "delete_cmd":
             cmd_selections = await get_all_user_commands(interaction.user.id)
             await interaction.response.edit_message(content = "", view = CmdMenu(cmd_selections, "delete")) 
+        elif action == "history_cmd":
+            cmd_selections = await find_top_messages(interaction, interaction.user, 10, None)
+            await interaction.response.edit_message(content = "", view = HistoryMenu(cmd_selections)) 
         
 class ModalForCmd(discord.ui.Modal):
-    def __init__(self):
+    def __init__(self, cmd_name : str = None):
         super().__init__(title = "Form to Save a Command")
         
         # Add the form for the command name 
@@ -174,6 +213,7 @@ class ModalForCmd(discord.ui.Modal):
         self.cmd_output_form = discord.ui.TextInput(
             label = "Command Output",
             placeholder = "Enter output for the command",
+            default = cmd_name,
             required = True
         )
         self.add_item(self.cmd_output_form)
@@ -253,6 +293,45 @@ async def delete_menu_callback(interaction : discord.Interaction):
     #send feedback to the user
     await interaction.response.send_message(f"Command {cmd_name} deleted: {result}", ephemeral = True)
 
+class HistoryMenu(discord.ui.View):
+    def __init__(self, message_history):
+        super().__init__()
+        
+        #check if any messages are in the list first
+        if len(message_history) == 0:
+            select_options = [discord.SelectOption(label = "No messages found", value = "No messages")]
+        else:
+            select_options = [
+                discord.SelectOption(label = f"Used {num} times: {label}", value=label)
+                for label , num in message_history
+            ]
+            
+        select = discord.ui.Select(
+            placeholder = "Click on message to save it as a command",
+            min_values=1,
+            max_values=1,
+            options=select_options
+        )
+        
+        #attach proper callback
+        if len(message_history) == 0:
+            select.callback = history_error
+        else:
+            select.callback = history_selection
+        
+        self.add_item(select)
+        
+    @discord.ui.button(label="", row = 1, style=discord.ButtonStyle.primary, emoji="⬅️") 
+    async def return_to_main(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.edit_message(content = "", view = MainMenu()) 
+
+async def history_error(interaction : discord.Interaction):
+    await interaction.response.send_message("Please save a command first.", ephemeral = True)
+
+async def history_selection(interaction: discord.Interaction):
+    cmd_name = interaction.data["values"][0]
+    await interaction.response.send_modal(ModalForCmd(cmd_name))
+
 @client.tree.command(name = "main_menu", description = "Calls the main menu", guild = GUILD_NUM)
 async def send_main_menu(interaction: discord.Interaction):
     await interaction.response.send_message("", view = MainMenu())  # Send a message with the View
@@ -312,27 +391,8 @@ async def searchHistory(interaction : discord.Interaction, count: int, date : st
         return
     
     user = interaction.user
-    # Create the dictionary to hold messages
-    message_dict = dict()
-    
-    num_msg = 0
-    # Fetch all messages in the channel after date and organize by first letter
-    async for message in interaction.channel.history(limit=None, after=after_date):  
-        if message.author == user:
-            
-            if message in message_dict:
-                message_dict[message.content] += 1
-            else:
-                message_dict[message.content] = 1
-                
-            num_msg += 1
-            #hard limit to avoid too much sorting
-            if num_msg == 1000: 
-                break
-    
-    # sort the dict and then get the top items with the highest values
-    messages = sorted(message_dict.items(), key=lambda item: item[1], reverse=True)[:count]
-    
+    messages = await find_top_messages(interaction, user, count, after_date)
+    print(messages)
     if messages:
         response = "\n".join([f"Used {msg[1]} times: {msg[0]}" for msg in messages])
         add_on = f" after {date}" if date else ""
